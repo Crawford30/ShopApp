9163b687a5b52c995fade9088517efe8270255a4
Joel Crawford <macbook@Macbooks-MacBook-Pro.local>
2022-01-26 13:19:15 +0300

    Added Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-
-    )
-  );
-
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
-
-
-
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -29,9 +22,39 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.red,
         title: const Text('Fashapp'),
         actions: [
-          new  IconButton( icon: Icon(Icons.search, color: Colors.white,), onPressed: (){}),
-          new  IconButton( icon: Icon(Icons.shopping_cart, color: Colors.white,), onPressed: (){})
+          IconButton(
+              icon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: const Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {})
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            //====Header Part of drawer====
+            UserAccountsDrawerHeader(
+                accountName: Text("Joel Crawford"),
+                accountEmail: Text("joelcrawford30@gmail.com"),
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person, color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+
+          ],
+        ),
       ),
     );
   }
