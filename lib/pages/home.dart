4b115b0e8ff8c7a6bd83dfe7f22e8426d45659c0
Joel Crawford <macbook@Macbooks-MacBook-Pro.local>
2022-02-01 16:51:45 +0300

    Google sign in

---
+++ lib/pages/home.dart
@@ -0,0 +1,196 @@
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/material.dart';
+import 'package:shopapp/components/horizontal_listview.dart';
+import 'package:shopapp/components/products.dart';
+import 'package:shopapp/pages/cart.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    Widget imageCarousel = SizedBox(
+      height: 200.0,
+      child: Carousel(
+        boxFit: BoxFit.cover,
+        images: const [
+          AssetImage('images/c1.jpg'),
+          AssetImage('images/m1.jpeg'),
+          AssetImage('images/m2.jpg'),
+          AssetImage('images/w1.jpeg'),
+          AssetImage('images/w3.jpeg'),
+          AssetImage('images/w4.jpeg'),
+        ],
+        autoplay: false,
+        // animationCurve: Curves.fastLinearToSlowEaseIn,
+        // animationDuration: Duration(milliseconds: 1000),
+        dotSize: 4.0,
+        indicatorBgPadding: 2.0,
+        dotBgColor: Colors.transparent,
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.1,
+        backgroundColor: Colors.red,
+        title: const Text('Fashapp'),
+        actions: [
+          IconButton(
+              icon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: const Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Cart()));
+              })
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            //====Header Part of drawer====
+            UserAccountsDrawerHeader(
+              accountName: Text("Joel Crawford"),
+              accountEmail: Text("joelcrawford30@gmail.com"),
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(color: Colors.pink),
+            ),
+
+            //=======Body========
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Home'),
+                leading: Icon(
+                  Icons.home,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('My Account'),
+                leading: Icon(
+                  Icons.person,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('My Orders'),
+                leading: Icon(
+                  Icons.shopping_basket,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Cart()));
+              },
+              child: ListTile(
+                title: Text('Shopping Cart'),
+                leading: Icon(
+                  Icons.shopping_cart,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Favourites'),
+                leading: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+
+            Divider(),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('Settings'),
+                leading: Icon(
+                  Icons.settings,
+                ),
+              ),
+            ),
+
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text('About'),
+                leading: Icon(
+                  Icons.help,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          //=======Image Carousel begins here =====
+          imageCarousel,
+
+          //========Padding widget=====
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Container(
+                alignment: Alignment.centerLeft,
+                child: Text("Categories")),
+          ),
+
+          //======Horizontal List View begins here =====
+          HorizontalList(),
+
+          //========Padding widget=====
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+                alignment: Alignment.centerLeft,
+                child: Text("Recent Products")),
+          ),
+
+          //=====Grid View
+
+          Flexible(child: Products()),
+
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,203 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'home.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  late SharedPreferences preferences;
+  bool loading = false;
+  bool isLoggedIn = false;
+
+  @override
+  void initState() {
+    // Calls the initial state of the class
+    super.initState();
+    isSignedIn();
+  }
+
+  void isSignedIn() async {
+    //=https://stackoverflow.com/questions/54445610/the-method-signinwithgoogle-isnt-defined-for-the-class-firebaseauth
+
+    //====Set State===
+    setState(() {
+      loading = true;
+    });
+    //========We assigning the instance of the shared preferences. await is a future method.(Hold on until it gets the value from the server)
+    preferences = await SharedPreferences.getInstance();
+
+    //Check if the user is signed in or not
+    isLoggedIn = await googleSignIn.isSignedIn();
+
+    //===If its true
+    if (isLoggedIn) {
+      //===With push replacement, the user doesnt have the ability to hit the back button to go back to the previous screen(login in scrrent)
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    }
+
+    //===Set state of loading because the user is logged in
+    setState(() {
+      loading = false;
+    });
+  }
+
+  //======If not logged in===
+  Future handleSignIn() async {
+    //await Firebase.initializeApp();
+    preferences = await SharedPreferences.getInstance();
+    setState(() {
+      loading = true;
+    });
+
+    // try {
+
+    //======Create google sign in account===
+
+    GoogleSignInAccount? googleUser =
+        await googleSignIn.signIn(); //sign the user in
+
+    GoogleSignInAuthentication googleSignInAuthentication =
+        await googleUser!.authentication; //authenticate the user
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+
+    // FirebaseUser firebaseUser = await firebaseAuth.signInWithGoogle(
+    //     accessToken: googleSignInAuthentication.accessToken,
+    //     idToken: googleSignInAuthentication.idToken);
+
+    UserCredential authResult =
+        await firebaseAuth.signInWithCredential(credential);
+
+    if (authResult.user != null) {
+      //===look for the user if the firebbase id is equal to the uid
+      final QuerySnapshot result = await FirebaseFirestore.instance
+          .collection("users")
+          .where("id", isEqualTo: authResult.user?.uid)
+          .get();
+
+      final List<DocumentSnapshot> documents = result.docs;
+
+      //====If the user doesnt exist on our collection
+      if (documents.length == 0) {
+        //insert the user to out collection
+        FirebaseFirestore.instance
+            .collection("users")
+            .doc(authResult.user?.uid)
+            .set({
+          "id": authResult.user!.uid,
+          "username": authResult.user!.displayName,
+          "profilePicture": authResult.user!.photoURL
+        });
+
+        //====Save results to shared preferences===
+        await preferences.setString("id", authResult.user!.uid);
+        await preferences.setString(
+            "username", authResult.user!.displayName.toString());
+        await preferences.setString(
+            "photourl", authResult.user!.photoURL.toString());
+      } else {
+        //====If the user does exist, take the data from documents and save to sharedpreferences===
+        await preferences.setString("id", documents[0]['id']);
+        await preferences.setString("username", documents[0]['username']);
+        await preferences.setString("photourl", documents[0]['photourl']);
+
+        //===Show toast if successfully done
+        Fluttertoast.showToast(msg: "Login was successful");
+
+        //===Set loading state
+        setState(() {
+          loading = false;
+        });
+
+        //===Go back to homepage===
+
+      }
+    } else {}
+
+    //  GoogleSignInAccount? account = await googleSignIn.signIn();
+    //
+    //  GoogleSignInAuthentication? authentication =
+    //     await account?.authentication;
+    //
+    //  OAuthCredential credential = GoogleAuthProvider.credential(
+    //     idToken: authentication?.idToken,
+    //     accessToken: authentication?.accessToken);
+    //
+    //  UserCredential authResult =
+    //     await firebaseAuth.signInWithCredential(credential);
+    //
+    // if(authResult.user != null){
+    //   //===Check if a user is signed out ===
+    //   final QuerySnapshot result = await FirebaseFirestore.instance.collection("users").where("id", isEqualTo, authResult.user.uid).getDocuments();
+    //
+    //
+    // } else {
+    //
+    // }
+    // final User? user = authResult.user;
+    //
+    // return user;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          "Login",
+          style: TextStyle(
+            color: Colors.red.shade900,
+          ),
+        ),
+        elevation: 0.1,
+      ),
+      body: Stack(
+        children: [
+          Center(
+            child: FlatButton(
+              color: Colors.red.shade900,
+              onPressed: () {
+                handleSignIn();
+              },
+              child: Text(
+                "Sign in/Sign up with google",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Visibility(
+            visible: loading ?? true,
+            child: Center(
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.white.withOpacity(0.9),
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(Colors.red),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
